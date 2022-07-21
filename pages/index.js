6f257f37c5a3a9f6705dd742547268be47866893
hakyung4 <hakyung4@illinois.edu>
2022-07-20 22:38:01 -0500

    featured posts

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { PostCard, PostWidget, Categories } from '../Components'
 import { getPosts } from '../services'
+import FeaturedPosts from '../sections/FeaturedPosts'
 
 export default function Home({ posts }) {
   return (
@@ -9,6 +10,7 @@ export default function Home({ posts }) {
       <Head>
         <title>Hakyung Yun Blog</title>
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
