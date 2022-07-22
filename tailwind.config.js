3d64decccd9d5b12f4146e4bb84e7762002c7295
hakyung4 <hakyung4@illinois.edu>
2022-07-21 22:50:17 -0500

    fix tailwind config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
+// module.exports = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx}",
+//     "./components/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
